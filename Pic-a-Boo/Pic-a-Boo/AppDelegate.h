116ca81c47f56ef9bba309ba5fccf9ccb104cea6
Jackson Sierra <jks718@gmail.com>
2016-02-23 15:19:56 -0800

    [setup] (ios) create tabbed application template

---
+++ Pic-a-Boo/Pic-a-Boo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pic-a-Boo
+//
+//  Created by Jackson Sierra on 2/23/16.
+//  Copyright © 2016 Jackson Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pic-a-Boo/Pic-a-Boo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pic-a-Boo
+//
+//  Created by Jackson Sierra on 2/23/16.
+//  Copyright © 2016 Jackson Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
