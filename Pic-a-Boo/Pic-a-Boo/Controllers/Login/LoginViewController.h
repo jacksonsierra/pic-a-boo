c78ef9d1b67ee80add483273db2084dbe453f509
Jackson Sierra <jks718@gmail.com>
2016-02-23 23:23:31 -0800

    [feature] (ios) add LoginViewController with associated IB outlets/actions

---
+++ Pic-a-Boo/Pic-a-Boo/Controllers/Login/LoginViewController.h
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.h
+//  Pic-a-Boo
+//
+//  Created by Jackson Sierra on 2/23/16.
+//  Copyright © 2016 Jackson Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController<NSURLConnectionDelegate> {
+  NSMutableData *requestData;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+- (IBAction)loginUser:(id)sender;
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response;
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
+
+@end
---
+++ Pic-a-Boo/Pic-a-Boo/Controllers/Login/LoginViewController.m
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.m
+//  Pic-a-Boo
+//
+//  Created by Jackson Sierra on 2/23/16.
+//  Copyright © 2016 Jackson Sierra. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+- (IBAction)loginUser:(id)sender {
+}
+@end
