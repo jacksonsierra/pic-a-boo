1e5ee1bc8138e00b1773b6b837b5a428cc0916af
Jackson Sierra <jks718@gmail.com>
2016-02-24 09:42:20 -0800

    [feature] (ios) add login functionality and URLConnection protocol straw man

--- Pic-a-Boo/Pic-a-Boo/Controllers/Login/LoginViewController.m
+++ Pic-a-Boo/Pic-a-Boo/Controllers/Login/LoginViewController.m
@@ -22,15 +22,61 @@
     [super didReceiveMemoryWarning];
 }
 
+- (IBAction)loginUser:(id)sender {
+  NSString *username = [self.usernameField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+  NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+  
+  // Alert user if username/password is empty
+  
+  // Initialize loading screen in center of view while identity is verified
+  // All loading screen to view
+  
+  // Initialize URLRequest object
+  
+  // Initialize URLConnection
+}
 
 #pragma mark - Navigation
-
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
 
 
-- (IBAction)loginUser:(id)sender {
+#pragma mark - NSURLConnection Protocol
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+  requestData = [[NSMutableData alloc] init];
+  
+  if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+    NSHTTPURLResponse *HTTPResponse = (NSHTTPURLResponse *) response;
+    NSInteger statusCode = HTTPResponse.statusCode;
+    NSLog(@"Status Code: %zd", statusCode);
+    
+    if (statusCode != 200) {
+      // Alert user of incorrect password
+      
+      [connection cancel];
+    }
+  }
 }
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+  [requestData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+  requestData = nil;
+  NSLog(@"Connection failed! Error - %@ %@",
+        [error localizedDescription],
+        [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+  NSString *dataString = [[NSString alloc] initWithData:(NSData *)requestData encoding:NSUTF8StringEncoding];
+  NSLog(@"%@", dataString);
+  requestData = nil;
+  
+  // Handle navigtaion
+}
+  
 @end
